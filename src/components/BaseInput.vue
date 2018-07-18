ae44ac828ee32560a1707e3b52e939e3bb529f18
Victor Nascimento <victormota15@gmail.com>
2018-07-18 03:22:42 -0300

    Add BaseInput component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="form-control">
+    <h2 class="label is-light">{{ label }}:</h2>
+    <input v-bind="$attrs" :value="value" @input="updateValue">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value', 'label'],
+  methods: {
+    updateValue ($ev) {
+      this.$emit('input', $ev.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~@/styles/variables';
+
+  .label {
+    margin-bottom: 0;
+  }
+
+  input {
+    border: none;
+    border-bottom: 2px solid $light;
+    background: transparent;
+    color: $light;
+    outline: none;
+    transition: all .3s ease;
+    font-size: 1.5em;
+    text-align: center;
+    padding: 1em;
+    flex: 1;
+    &:focus {
+      border-bottom-color: $primary;
+    }
+  }
+
+  .form-control {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
