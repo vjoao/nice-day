6b6b46d7d1340994f082ed19fd42f1849eb32d07
Victor Nascimento <victormota15@gmail.com>
2018-07-18 03:23:47 -0300

    Register global components

---
+++ src/components/TheLoader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="loader"></div>
+</template>
+
+<style lang="scss">
+  @import '~@/styles/loader';
+</style>
---
+++ src/globalComponents.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import TheLoader from './components/TheLoader'
+import BaseInput from './components/BaseInput'
+
+Vue.component('the-loader', TheLoader)
+Vue.component('base-input', BaseInput)
