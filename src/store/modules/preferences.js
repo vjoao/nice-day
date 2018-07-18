40eb1f7d0c03ece88e612c0a1b4f9954a54cc2d8
Victor Nascimento <victormota15@gmail.com>
2018-07-18 03:28:52 -0300

    Add user preferences module

---
+++ src/store/modules/preferences.js
@@ -0,0 +1,37 @@
+import { SET_PREFERENCES } from '../mutationTypes'
+
+export default {
+  namespaced: true,
+  state: {
+    rainIntensity: 0,
+    temperatureMin: 0,
+    temperatureMax: 0,
+    windSpeed: 0,
+    hasBeenSet: false
+  },
+  mutations: {
+    SET_PREFERENCES (state, preferences) {
+      Object.keys(preferences).forEach(key => {
+        state[key] = preferences[key]
+      })
+      state.hasBeenSet = true
+    }
+  },
+  actions: {
+    setPreferences ({ commit }, preferences) {
+      const { rainIntensity, temperatureMin, temperatureMax, windSpeed } = preferences
+
+      commit(SET_PREFERENCES, {
+        rainIntensity,
+        temperatureMin,
+        temperatureMax,
+        windSpeed
+      })
+
+      commit('auth/SET_API_KEY', preferences.apiKey, { root: true })
+    }
+  },
+  getters: {
+
+  }
+}
