dc0cdabd6d888109aaf5abdde8230300ffa15145
Victor Nascimento <victormota15@gmail.com>
2018-07-18 03:23:06 -0300

    Add SVG support

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="currentIcon"></component>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      currentIcon: require(`@/assets/${this.name}.svg`)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
