e4a5806224a4c20de1da4025611f8cc47ac8e9c1
Victor Nascimento <victormota15@gmail.com>
2018-07-18 03:22:51 -0300

    Add IconBox component

---
+++ src/components/IconBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col align-center">
+    <svg-icon :name="icon"></svg-icon>
+    <h2 class="text-center is-light" :class="{ 'is-error': danger }">{{info}}</h2>
+  </div>
+</template>
+
+<script>
+import SvgIcon from '@/components/SvgIcon'
+
+export default {
+  components: {
+    SvgIcon
+  },
+  props: {
+    icon: String,
+    info: String,
+    danger: Boolean
+  }
+}
+</script>
